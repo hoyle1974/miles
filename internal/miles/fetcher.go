6076d467006959a258f0ce11dcaa2302c02465a4
Jack Strohm <hoyle.hoyle@gmail.com>
2024-06-25 19:58:48 -0700

    updates

--- internal/miles/fetcher.go
+++ internal/miles/fetcher.go
@@ -9,11 +9,13 @@ import (
 )
 
 // HTML Fetcher: HTML fetcher component is responsible for downloading web pages corresponding to a given URL provided by the URL Frontier. It does this by using a network protocol like HTTP or HTTPS. In simple words, HTML fetcher retrieves the actual web page content that needs to be analyzed and stored.
+var docStore = store.NewDocStore()
 
 func FetchURL(URL url.Nurl) ([]byte, error) {
-	data, err := store.GetKVStore().Get(URL.String())
-	if data != nil {
-		return data, nil
+
+	doc, err := docStore.GetDoc(URL)
+	if doc != nil {
+		return doc.GetData(), doc.GetError()
 	}
 
 	// Create a new HTTP client
@@ -22,28 +24,35 @@ func FetchURL(URL url.Nurl) ([]byte, error) {
 	// Create a new GET request
 	req, err := http.NewRequest(http.MethodGet, URL.String(), nil)
 	if err != nil {
-		return nil, fmt.Errorf("NewRequest %w", err)
+		err = fmt.Errorf("NewRequest %w", err)
+		docStore.Store(URL, nil, err)
+		return nil, err
 	}
 
 	// Do the request
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("DoRequest %w", err)
+		err = fmt.Errorf("DoRequest %w", err)
+		docStore.Store(URL, nil, err)
+		return nil, err
 	}
 	defer resp.Body.Close() // Close the response body after use
 
 	// Check for successful response status code
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		err = fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		docStore.Store(URL, nil, err)
+		return nil, err
 	}
 
 	// Read the response body into a byte buffer
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("ReadAll %w", err)
+		err = fmt.Errorf("ReadAll %w", err)
+		docStore.Store(URL, nil, err)
+		return nil, err
 	}
 
-	store.GetKVStore().Put(URL.String(), body)
-
+	docStore.Store(URL, body, nil)
 	return body, nil
 }
--- internal/store/docstore.go
+++ internal/store/docstore.go
@@ -1,6 +1,22 @@
 package store
 
+import (
+	"bytes"
+	"encoding/gob"
+	"github.com/hoyle1974/miles/internal/url"
+)
+
 type Doc struct {
+	Data  []byte
+	Error error
+}
+
+func (d Doc) GetData() []byte {
+	return d.Data
+}
+
+func (d Doc) GetError() error {
+	return d.Error
 }
 
 type DocStore struct {
@@ -8,5 +24,39 @@ type DocStore struct {
 }
 
 func NewDocStore() DocStore {
+	gob.Register(Doc{})
 	return DocStore{kvStore: NewKVStore("./badgerdb")}
+}
+
+func (ds DocStore) GetDoc(nurl url.Nurl) Doc {
+	key := nurl.String()
+
+	data, err := ds.kvStore.Get(key)
+	if err != nil {
+		return Doc{Error: err}
+	}
+
+	buf := bytes.NewBuffer(data)
+
+	dec := gob.NewDecoder(buf)
+	var doc Doc
+	err = dec.Decode(&doc)
+	if err != nil {
+		return Doc{Error: err}
+	}
+	return doc
+}
+
+func (ds DocStore) Store(nurl url.Nurl, data []byte, err error) error {
+	key := nurl.String()
+	doc := Doc{Data: data, Error: err}
+
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err = enc.Encode(doc)
+	if err != nil {
+		return err
+	}
+
+	return ds.kvStore.Put(key, buf.Bytes())
 }
